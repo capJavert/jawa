1084a4789540e433ec6da556381bd281fb207b99
capJavert <ante@kickass.website>
2022-10-17 01:17:37 +0200

    feat: add basic layout

---
+++ components/ColorScheemToggle.tsx
@@ -0,0 +1,37 @@
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded'
+import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded'
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded'
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded'
+import IconButton from '@mui/joy/IconButton'
+import { useColorScheme } from '@mui/joy/styles'
+import { useEffect, useState } from 'react'
+
+const ColorSchemeToggle = () => {
+    const { mode, setMode } = useColorScheme()
+    const [mounted, setMounted] = useState(false)
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+    if (!mounted) {
+        return <IconButton size="sm" variant="outlined" color="primary" />
+    }
+    return (
+        <IconButton
+            id="toggle-mode"
+            size="sm"
+            variant="outlined"
+            color="primary"
+            onClick={() => {
+                if (mode === 'light') {
+                    setMode('dark')
+                } else {
+                    setMode('light')
+                }
+            }}
+        >
+            {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+        </IconButton>
+    )
+}
+
+export default ColorSchemeToggle
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,70 @@
 import '../styles/globals.css'
 
+import FaceIcon from '@mui/icons-material/Face'
+import Box from '@mui/joy/Box'
+import IconButton from '@mui/joy/IconButton'
+import type { Theme } from '@mui/joy/styles'
+import { CssVarsProvider } from '@mui/joy/styles'
+import Typography from '@mui/joy/Typography'
+import { GlobalStyles } from '@mui/system'
 import type { AppProps } from 'next/app'
 
+import ColorSchemeToggle from '../components/ColorScheemToggle'
+import Layout from '../components/Layout'
+import theme from '../theme'
+
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <CssVarsProvider>
+            <CssVarsProvider disableTransitionOnChange theme={theme}>
+                <GlobalStyles<Theme>
+                    styles={theme => ({
+                        body: {
+                            margin: 0,
+                            fontFamily: theme.vars.fontFamily.body
+                        }
+                    })}
+                />
+                <Layout.Container>
+                    <Layout.Header>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                gap: 1.5
+                            }}
+                        >
+                            <IconButton
+                                tabIndex={-1}
+                                size="sm"
+                                variant="solid"
+                                sx={{ display: { xs: 'none', sm: 'inline-flex' } }}
+                            >
+                                <FaceIcon />
+                            </IconButton>
+                            <Typography component="h1" fontWeight="xl">
+                                JAWA visual scraper
+                            </Typography>
+                        </Box>
+                        <Box
+                            id="header-top-bar"
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                flexBasis: 600
+                            }}
+                        />
+                        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5 }}>
+                            <ColorSchemeToggle />
+                        </Box>
+                    </Layout.Header>
+                    <Component {...pageProps} />
+                </Layout.Container>
+            </CssVarsProvider>
+        </CssVarsProvider>
+    )
 }
 
 export default MyApp
