0aa64f6185e061b22cc4a46d6834a4346bbc0391
capJavert <ante@kickass.website>
2022-10-17 03:02:39 +0200

    chore: cleanup

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,4 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message)
-
     switch (message.type) {
         case 'init': {
             if (sender.tab) {
