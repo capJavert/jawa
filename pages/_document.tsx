e6c5cacbe75cd39f39327ae5ca18c1d8ffbbce8a
capJavert <ante@kickass.website>
2022-10-19 17:35:04 +0200

    fix(a11y): add lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
 class Document extends NextDocument {
     render() {
         return (
-            <Html>
+            <Html lang="en">
                 <Head>
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
