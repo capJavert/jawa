bd8dea2040e482fe31b48dacb1f760e77399c742
Ante Barić <ante@kickass.website>
2023-10-14 15:10:24 +0200

    feat: cloud ui enhancements (#13)

--- components/DownloadModal.tsx
+++ components/DownloadModal.tsx
@@ -1,9 +1,33 @@
-import { SendAndArchive as SendAndArchiveIcon } from '@mui/icons-material'
-import { Badge, Box, Button, FormControl, Input, Modal, ModalClose, ModalDialog, Typography } from '@mui/joy'
+import {
+    Check as CheckIcon,
+    Error as ErrorIcon,
+    HourglassBottom as HourglassBottomIcon,
+    SendAndArchive as SendAndArchiveIcon
+} from '@mui/icons-material'
+import {
+    Alert,
+    AspectRatio,
+    Badge,
+    Box,
+    Button,
+    ColorPaletteProp,
+    FormControl,
+    IconButton,
+    Input,
+    LinearProgress,
+    Link as MuiLink,
+    Modal,
+    ModalClose,
+    ModalDialog,
+    Typography
+} from '@mui/joy'
 import Link from 'next/link'
 // @ts-ignore
 import * as platformDetect from 'platform-detect'
 import { useMemo, useState } from 'react'
+
+import { EScraperErrorMessage } from '../types'
+import { getShortHash, promptFileDownload } from '../utils'
 
 const DownloadModal = ({
     download,
@@ -14,9 +38,11 @@ const DownloadModal = ({
     download: string | false
     onClose: (event: {}, reason: 'backdropClick' | 'escapeKeyDown' | 'closeClick') => void
     onSubmit: () => void
-    onRun: () => Promise<void>
+    onRun: () => Promise<Record<string, any>>
 }) => {
     const [isRunning, setRunning] = useState(false)
+    const [result, setResult] = useState<any>()
+    const hasError = result instanceof Error
 
     const downloadFolder = useMemo(() => {
         if (typeof window === 'undefined') {
@@ -47,6 +73,93 @@ const DownloadModal = ({
         }
     }, [])
     const codeRunSnippet = `npx jawa ${downloadFolder}vscraper-config-${download}.json`
+
+    const alertColor = useMemo<ColorPaletteProp>(() => {
+        if (isRunning) {
+            return 'primary'
+        }
+
+        if (hasError) {
+            if (result.message === EScraperErrorMessage.timeout) {
+                return 'neutral'
+            }
+
+            return 'danger'
+        }
+
+        return 'success'
+    }, [result, hasError, isRunning])
+
+    const AlertIcon = useMemo(() => {
+        if (isRunning) {
+            return HourglassBottomIcon
+        }
+
+        if (hasError) {
+            return ErrorIcon
+        }
+
+        return CheckIcon
+    }, [hasError, isRunning])
+
+    const alertContent = useMemo(() => {
+        if (isRunning) {
+            return (
+                <Typography level="body2">
+                    Running the scraper in the Cloud, this can take some time. Please leave this page open (or in
+                    background)...
+                </Typography>
+            )
+        }
+
+        if (hasError) {
+            if (result.message === EScraperErrorMessage.timeout) {
+                return (
+                    <Typography level="body2">
+                        Page scraping failed due to timeout. During Beta period we have a limit of 10 seconds for
+                        execution duration. If you need more time or wish to support the project{' '}
+                        <Link target="_blank" href={process.env.NEXT_PUBLIC_CONTACT_HREF as string}>
+                            <Typography color="primary" component="u">
+                                contact us
+                            </Typography>
+                        </Link>{' '}
+                        or{' '}
+                        <Link target="_blank" href={process.env.NEXT_PUBLIC_SPONSOR_HREF as string}>
+                            <Typography color="primary" component="u">
+                                sponsor with Jawa Pro
+                            </Typography>
+                        </Link>
+                    </Typography>
+                )
+            }
+
+            return (
+                <Typography level="body2">
+                    Something went wrong, please try again later. Jawas also logged the error and will look into it.
+                </Typography>
+            )
+        }
+
+        return (
+            <Typography level="body2">
+                The page was scraped successfully, results download should have triggered automatically, if not{' '}
+                <MuiLink
+                    underline="always"
+                    onClick={() => {
+                        const resultJSON = JSON.stringify(result)
+                        const resultHash = getShortHash({ payload: resultJSON })
+
+                        promptFileDownload({
+                            fileName: `vscraper-results-${resultHash}.json`,
+                            content: resultJSON
+                        })
+                    }}
+                >
+                    click here
+                </MuiLink>
+            </Typography>
+        )
+    }, [result, hasError, isRunning])
 
     return (
         <Modal
@@ -141,11 +254,14 @@ const DownloadModal = ({
                             title="Download config"
                             onClick={async () => {
                                 try {
+                                    setResult(undefined)
                                     setRunning(true)
 
-                                    await onRun()
+                                    const result = await onRun()
+
+                                    setResult(result)
                                 } catch (error) {
-                                    console.error(error)
+                                    setResult(error)
                                 } finally {
                                     setRunning(false)
                                 }
@@ -158,13 +274,55 @@ const DownloadModal = ({
                     </Badge>
                 </Box>
 
+                {(result || isRunning) && (
+                    <Alert
+                        sx={{ alignItems: 'center', justifyContent: 'center', overflow: 'hidden', marginBottom: 2 }}
+                        size="lg"
+                        color={alertColor}
+                        variant="solid"
+                        invertedColors
+                        startDecorator={
+                            <AspectRatio
+                                variant="solid"
+                                ratio="1"
+                                sx={{
+                                    minWidth: 30,
+                                    borderRadius: '50%',
+                                    boxShadow: '0 2px 12px 0 rgb(0 0 0/0.2)'
+                                }}
+                            >
+                                <div>
+                                    <AlertIcon fontSize="small" />
+                                </div>
+                            </AspectRatio>
+                        }
+                    >
+                        <Box>{alertContent}</Box>
+                        {isRunning && (
+                            <LinearProgress
+                                variant="soft"
+                                value={40}
+                                sx={theme => ({
+                                    position: 'absolute',
+                                    bottom: 0,
+                                    left: 0,
+                                    right: 0,
+                                    color: `rgb(${theme.vars.palette.success.lightChannel} / 0.72)`,
+                                    '--LinearProgress-radius': '0px'
+                                })}
+                            />
+                        )}
+                    </Alert>
+                )}
+
                 <Typography
                     level="body2"
                     sx={{
                         marginBottom: 2
                     }}
                 >
-                    You can run config locally with command below after download, only requirement is that you have{' '}
+                    You can {result ? 'also ' : ''}run config locally with command below after download, only
+                    requirement is that you have{' '}
                     <Link
                         href="https://docs.npmjs.com/downloading-and-installing-node-js-and-npm"
                         target="_blank"
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,6 @@
+// @ts-ignore
+import * as sha1 from 'js-sha1'
+
 export const waitForReCaptcha = async (): Promise<void> => {
     await new Promise(resolve => {
         window.grecaptcha.ready(() => {
@@ -12,4 +15,35 @@ export const getReCaptchaToken = async (): Promise<string> => {
     const token = await window.grecaptcha.execute(process.env.NEXT_PUBLIC_RECAPTCHA_KEY as string, { action: 'submit' })
 
     return token
+}
+
+export const getShortHash = ({ payload }: { payload: unknown }): string => {
+    const sha1Instance = sha1.create()
+    sha1Instance.update(JSON.stringify(payload))
+    const hash = sha1Instance.hex() as string
+    const shortHash = hash.substring(0, 10)
+
+    return shortHash
+}
+
+export const promptFileDownload = ({
+    fileName,
+    content,
+    type = 'application/json'
+}: {
+    fileName: string
+    content: BlobPart
+    type?: string
+}) => {
+    const aElement = document.createElement('a')
+    aElement.setAttribute('download', fileName)
+    const href = URL.createObjectURL(
+        new Blob([content], {
+            type
+        })
+    )
+    aElement.href = href
+    aElement.setAttribute('target', '_blank')
+    aElement.click()
+    URL.revokeObjectURL(href)
 }
