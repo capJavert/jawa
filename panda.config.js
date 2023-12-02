f9680e0231934c94e79b920c12636274735a84eb
capJavert <ante@kickass.website>
2023-12-03 00:56:07 +0100

    feat: add panda and park ui setup

---
+++ panda.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from '@pandacss/dev'
+
+export default defineConfig({
+    preflight: true,
+    presets: ['@pandacss/preset-base', '@park-ui/panda-preset'],
+    include: ['./src/**/*.{js,jsx,ts,tsx}'],
+    exclude: [],
+    theme: {
+        extend: {}
+    },
+    jsxFramework: 'react',
+    outdir: 'panda-styled-system'
+})
